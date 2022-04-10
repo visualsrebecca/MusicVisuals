3e7885979978e2e892129cfc7cc3dc1ce2192825
Rebecca <C20301121@mytudublin.ie>
2022-04-10 14:35:51 +0100

    adding song to project

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,8 +1,8 @@
 package ie.tudublin;
 
-import example.CubeVisual;
+//import example.CubeVisual;
 import example.MyVisual;
-import example.RotatingAudioBands;
+//import example.RotatingAudioBands;
 
 public class Main
 {	
